d9dedecbe318d573eeca353d66c6d2ccb28f9699
oscarka <ka-0724@hotmail.com>
2023-06-13 22:57:17 +0800

    Update tool.py

--- plugins/tool/tool.py
+++ plugins/tool/tool.py
@@ -14,13 +14,6 @@ from common.log import logger
 from config import conf
 from plugins import *
 
-06/13
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
-
-
 
 @plugins.register(
     name="tool",
